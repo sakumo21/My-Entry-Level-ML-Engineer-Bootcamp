e3bb14068dfc55d4da7a0e134492e3ef5a73c63c
sakumo21 <mouad.fdofus@gmail.com>
2025-03-02 23:17:54 +0000

    Week00 is completed !

--- Week00/ex07/scrapper.py
+++ Week00/ex07/scrapper.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = "https://data.1337ai.org"
 r = requests.get(URL)
-print(type(r.content))
 soup = BeautifulSoup(r.content, 'html5lib')
-print(soup.find_all('th'))
-# print(soup.body.tr.children)
+with open("data.csv", "w") as file:
+    for item in soup.find_all("tr"):
+        file.write(",".join([x.text for x in item]) + '\n')
+
