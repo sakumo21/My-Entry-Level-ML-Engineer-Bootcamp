d22ff8d1a91626aa03bc58500e5d4355c08c7df7
sakumo21 <mouad.fdofus@gmail.com>
2025-04-09 10:40:49 +0100

    ex05 in progress

--- week01/test.py
+++ week01/test.py
@@ -1,10 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import seaborn as sns
+import pandas
 
-# fig = plt.figure()             # an empty figure with no Axes
-# fig, ax = plt.subplots()       # a figure with a single Axes
-# fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
-# # a figure with one Axes on the left, and two on the right:
-fig, axs = plt.subplot_mosaic([['left', 'right_top'],
-                               ['left', 'right_bottom']])
-plt.savefig("plot.png")
+df = pandas.read_csv('Housing.csv')
+cleaned_df = df.drop_duplicates()
+cleaned_df = cleaned_df.dropna()
+columns = ["price", "area", "bedrooms", "bathrooms", "stories", "parking"]
+columns2 = ["mainroad", "guestroom", "basement", "hotwaterheating", "airconditioning", "prefarea", "furnishingstatus"]
+
+#============>histogram plots<============
+
+fig1, axs = plt.subplots(2, 3, figsize=(15, 8))
+axs = axs.flatten()
+
+for i, col in enumerate(columns):
+	sns.histplot(df[col], bins=20, kde=True, ax=axs[i])
+    
+plt.tight_layout()
+plt.savefig("plot.png") 
+plt.close(fig1)
+
+#============>boxplots<============
+
+fig2, axs2 = plt.subplots(2, 3, figsize=(15, 8))
+axs2 = axs2.flatten()
+
+for i, col in enumerate(columns):
+	sns.boxplot(data=df, x=col, ax=axs2[i])
+
+
+plt.tight_layout()
+plt.savefig("plot2.png") 
+plt.close(fig2)
+
+# #============>plot pie charts<============
+
+fig3, axs3 = plt.subplots(3, 3, figsize=(15, 15))
+axs3 = axs3.flatten()
+
+for i, col in enumerate(columns2):
+	counts = df[col].value_counts()
+	axs3[i].pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90)
+	axs3[i].set_title(f'Pie Chart of {col.capitalize()}')
+
+fig3.delaxes(axs3[7])
+fig3.delaxes(axs3[8])
+plt.tight_layout()
+plt.savefig("plot3.png")
+plt.close(fig3)
